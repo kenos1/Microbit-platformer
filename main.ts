cb4266e8ac1b097fa4e9942d3e7a1fcb7b963d42
yardent-was-taken <75338427+yardent-was-taken@users.noreply.github.com>
2021-01-19 14:44:40 -0700

    added side hitboxes

--- main.ts
+++ main.ts
@@ -1,33 +1,48 @@
 input.onButtonPressed(Button.A, function () {
+    led.unplot(playerx, playery)
+    if (led.point(playerx - 1, playery)) {
+        playerx += 1
+    }
     playerx += -1
+    led.plotBrightness(playerx, playery, 100)
 })
 input.onButtonPressed(Button.AB, function () {
-    playery = playery - 3
+    playery = playery - 2
 })
 input.onButtonPressed(Button.B, function () {
+    led.unplot(playerx, playery)
+    if (led.point(playerx + 1, playery)) {
+        playerx += -1
+    }
     playerx += 1
+    led.plotBrightness(playerx, playery, 100)
 })
 let gravity = 0
 let playerx = 0
 let playery = 0
-playery = 4
 playery = 3
+playerx = 2
 basic.forever(function () {
     basic.showLeds(`
         . . . . .
+        . . # # .
         . . . . .
-        . . . . .
-        . . . . .
+        # . . # #
         # # # # #
         `)
 })
 basic.forever(function () {
     led.unplot(playerx, playery)
+    if (!(gravity == 0)) {
+        basic.pause(100)
+        playery += gravity
+    }
+    led.plotBrightness(playerx, playery, 100)
+})
+basic.forever(function () {
     if (!(led.point(playerx, playery + 1))) {
         gravity = 1
     } else {
         gravity = 0
     }
-    playery += gravity
-    led.plot(playerx, playery)
 })
