103bb49c1c2934ba5b75ba124dd1ce660c1eceda
yardent-was-taken <75338427+yardent-was-taken@users.noreply.github.com>
2021-01-19 14:11:30 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,33 @@
 input.onButtonPressed(Button.A, function () {
     playerx += -1
 })
+input.onButtonPressed(Button.AB, function () {
+    playery = playery - 3
+})
 input.onButtonPressed(Button.B, function () {
     playerx += 1
 })
+let gravity = 0
 let playerx = 0
-let playery = 4
+let playery = 0
+playery = 4
 playery = 3
 basic.forever(function () {
-    led.plot(playerx, playery)
-    basic.pause(100)
-    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        `)
 })
 basic.forever(function () {
+    led.unplot(playerx, playery)
     if (!(led.point(playerx, playery + 1))) {
-    	
+        gravity = 1
     } else {
-    	
+        gravity = 0
     }
+    playery += gravity
+    led.plot(playerx, playery)
 })
