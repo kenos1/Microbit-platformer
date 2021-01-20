faf2c57b82763257c5925b1935f7b81dc807e356
yardent-was-taken <75338427+yardent-was-taken@users.noreply.github.com>
2021-01-19 19:17:06 -0700

    Game mechanics work

--- main.ts
+++ main.ts
@@ -7,7 +7,9 @@ input.onButtonPressed(Button.A, function () {
     led.plotBrightness(playerx, playery, 100)
 })
 input.onButtonPressed(Button.AB, function () {
-    playery = playery - 2
+    if (led.pointBrightness(playerx, playery + 1) == 255) {
+        playery = playery - 3
+    }
 })
 input.onButtonPressed(Button.B, function () {
     led.unplot(playerx, playery)
@@ -20,16 +22,24 @@ input.onButtonPressed(Button.B, function () {
 let gravity = 0
 let playerx = 0
 let playery = 0
+let level = 1
 playery = 3
-playerx = 2
+playerx = 0
 basic.forever(function () {
-    basic.showLeds(`
-        . . . . .
-        . . # # .
-        . . . . .
-        # . . # #
-        # # # # #
-        `)
+    if (4 == playerx) {
+        level += 1
+        playerx = 0
+    } else {
+        if (4 < playery) {
+            playerx = 0
+            playery += -3
+        } else {
+            if (-1 > playery) {
+                playery = 3
+                playerx = 0
+            }
+        }
+    }
 })
 basic.forever(function () {
     led.unplot(playerx, playery)
@@ -37,7 +47,111 @@ basic.forever(function () {
         basic.pause(100)
         playery += gravity
     }
+    basic.pause(50)
     led.plotBrightness(playerx, playery, 100)
+})
+basic.forever(function () {
+    if (level == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # # .
+            # # # # #
+            `)
+    } else {
+        if (level == 2) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                # # . # #
+                `)
+        } else {
+            if (level == 3) {
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . . # #
+                    . . # # #
+                    # # # # #
+                    `)
+            } else {
+                if (level == 4) {
+                    basic.showLeds(`
+                        . . . . .
+                        . . . . .
+                        . . . # .
+                        . . . # .
+                        # . # # #
+                        `)
+                } else {
+                    if (level == 5) {
+                        basic.showLeds(`
+                            . . . . .
+                            . . . . .
+                            . . # # .
+                            . . . # .
+                            # # # # #
+                            `)
+                    } else {
+                        if (level == 6) {
+                            basic.showLeds(`
+                                . . . # .
+                                . . . . .
+                                . . . . .
+                                . # . . .
+                                # # # . #
+                                `)
+                        } else {
+                            if (level == 7) {
+                                basic.showLeds(`
+                                    . . . . .
+                                    . . . . .
+                                    . . . . .
+                                    . . . # .
+                                    # # . . .
+                                    `)
+                            } else {
+                                if (level == 8) {
+                                    basic.showLeds(`
+                                        . . . . .
+                                        . . . . .
+                                        . . . # .
+                                        # # . . .
+                                        . . . . .
+                                        `)
+                                } else {
+                                    if (level == 9) {
+                                        basic.showLeds(`
+                                            . . . . .
+                                            . . . . .
+                                            . . . # .
+                                            # # . . .
+                                            . . . . .
+                                            `)
+                                    } else {
+                                        if (level == 10) {
+                                            basic.showLeds(`
+                                                . . . . .
+                                                . . . . #
+                                                . # . . .
+                                                . . . # .
+                                                # . . . .
+                                                `)
+                                        } else {
+                                            basic.showString("You beat the game!")
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 })
 basic.forever(function () {
     if (!(led.point(playerx, playery + 1))) {
